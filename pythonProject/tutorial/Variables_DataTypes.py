ecf5ccaa8a454d9af4b73c0503a6191aa9e1e275
abusafayatarefin92 <abusafayatarefin92@gmail.com>
2022-03-23 14:58:43 +0600

    Delete_Folder

---
+++ pythonProject/tutorial/Variables_DataTypes.py
@@ -0,0 +1,10 @@
+character_name = "John"
+character_age = 70
+
+print("There was a man named " + character_name + ".")
+print("He was " + str(character_age) + " years old")
+
+character_name = "Khan"
+character_age = 80
+print("He liked his name " + character_name + " very much")
+print("But he didn't like his " + str(character_age) + ".")
