77708e7acef159d698f8397c9c538e4529b294a2
abusafayatarefin92 <abusafayatarefin92@gmail.com>
2022-05-15 13:30:54 +0600

    form validation

---
+++ pythonProject/myproject/FlaskMarket/market/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '9a7accde76d95a972c97e4d0'
+db = SQLAlchemy(app)
+
+from market import routes
---
+++ pythonProject/myproject/FlaskMarket/market/routes.py
@@ -0,0 +1,43 @@
+from market import app
+from flask import render_template, redirect, url_for
+from market.models import Item, User
+from market.forms import RegisterForm
+from market import db
+
+
+@app.route('/about')
+def about_page():
+    return '<h1>About Page</h1>'
+
+
+@app.route('/about/<username>')
+def about_page_username(username):
+    return f'<h1>This is About Page of {username}</h1>'
+
+
+@app.route('/home')
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+
+@app.route('/market')
+def market_page():
+    items = Item.query.all()
+    return render_template('market.html', items=items)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_form():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:  # if there are npt errors from the validators
+        for err_msg in form.errors.values():
+            print(f'There is an error to create user {err_msg}')
+    return render_template('register.html', form=form)
